c599b602b1366be51b30f99b9f8361e946185107
mk-hasan <mkhasan6891@gmail.com>
2022-08-15 15:39:52 +0600

    data showed

--- src/component/Products/ProductCard.js
+++ src/component/Products/ProductCard.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 const ProductCard = ({ product }) => {
-    console.log(product);
-    const { id, name, price } = product;
+    const { id, name, title, category, price, description, shafe, date, popularity, tags, image, comments, likes } = product;
     return (
         <div>
-            <p>{name}</p>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={image} alt="product" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        Shoes!
+                        <div class="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <div class="badge badge-outline">Fashion</div>
+                        <div class="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
